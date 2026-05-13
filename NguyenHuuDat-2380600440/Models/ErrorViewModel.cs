9b57b83a3e33ee43d3fa85a4c45e50167215e215
Administrator <Administrator@10PC290>
2026-05-13 10:45:35 +0700

    Add project files.

---
+++ NguyenHuuDat-2380600440/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NguyenHuuDat_2380600440.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
